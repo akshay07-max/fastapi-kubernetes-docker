d4c5023296b7f41eb96bc74bc2d1a3e7329a8713
akshay07-max <akshaysurase992@gmail.com>
2026-04-14 15:00:25 +0530

    websocket connection setup

--- app/dummy_data.py
+++ app/dummy_data.py
@@ -1,7 +1,7 @@
 dummy_data = [
-    {"name": "Akshay", "age": 23, "year_of_birth": 2003, "education": "BE", "year_of_passing": 2025, "male": True},
-    {"name": "Mina", "age": 18, "year_of_birth": 2008, "education": "12th", "year_of_passing": 2025, "male": False},
-    {"name": "Hira", "age": 35, "year_of_birth": 1991, "education": "MD", "year_of_passing": 2018, "male": True},
-    {"name": "Kajal", "age": 24, "year_of_birth": 2001, "education": "BE", "year_of_passing": 2025, "male": False},
-    {"name": "Sonam", "age": 24, "year_of_birth": 2001, "education": "BE", "year_of_passing": 2025, "male": False}
+    {"id": 1, "name": "Akshay", "age": 23, "year_of_birth": 2003, "education": "BE", "year_of_passing": 2025, "male": True},
+    {"id": 2, "name": "Mina", "age": 18, "year_of_birth": 2008, "education": "12th", "year_of_passing": 2025, "male": False},
+    {"id": 3, "name": "Hira", "age": 35, "year_of_birth": 1991, "education": "MD", "year_of_passing": 2018, "male": True},
+    {"id": 4, "name": "Kajal", "age": 24, "year_of_birth": 2001, "education": "BE", "year_of_passing": 2025, "male": False},
+    {"id": 5, "name": "Sonam", "age": 24, "year_of_birth": 2001, "education": "BE", "year_of_passing": 2025, "male": False}
 ],
--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, websockets
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from dummy_data import dummy_data
 
 app = FastAPI()
 
@@ -7,3 +8,48 @@ def root():
     return {
             "message": "app started successfully"
             }
+
+
+@app.get("/data")
+def get_dummy_data(data):
+    return dummy_data
+
+# websocket 
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+manager = ConnectionManager()
+
+# websocket endpoint
+@app.websocket("/ws")
+async def websocket_endpoint(websocket:WebSocket):
+    await manager.connect()
+
+    try:
+        #send initial summy data
+        for message in dummy_data:
+            await websocket.send_json(message)
+
+        while True:
+            data = await websocket.receive_text()
+            response = f"Client says: {data}"
+            
+            # Broadcast to all clients
+            await manager.broadcast(response)
+
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+
